be5df7ac75607858cb9f50f12d14e2420eea4ab0
Gerald Nwanze <nwanzegerald@gmail.com>
2024-08-09 23:58:27 +0100

    setup postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,47 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: getEnvFilePath(),
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        synchronize: true,
+        url: configService.get('POSTGRES_URL'),
+        autoLoadEntities: true
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// Function to determine the environment file path dynamically based on some condition
+function getEnvFilePath(): string[] {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+    case 'prod':
+      return ['.env'];
+
+    case 'test':
+      return ['.env.test'];
+
+    case 'dev':
+    case 'staging':
+    case 'development':
+      return ['.env.dev'];
+
+    default:
+      return ['.env.dev'];
+  }
+}
